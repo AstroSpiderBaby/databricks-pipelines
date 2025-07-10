f0dbda692874d965e7226c284eff5a84be1f2e5c
Bruce Jenks <brucelv426@gmail.com>
2025-07-10 14:55:58 -0700

    Refactor: Convert inventory bronze notebook to Python script

---
+++ pipeline1_batch_delta/bronze_py/bronze_inventory_ingest.py
@@ -0,0 +1,22 @@
+"""
+bronze_inventory_ingest.py
+
+Ingests raw inventory CSV data from Blob Storage into Bronze Delta Lake.
+"""
+
+from pyspark.sql import SparkSession
+from utils.utils_write_delta import write_df_to_delta
+
+spark = SparkSession.builder.getOrCreate()
+
+inventory_path = "dbfs:/FileStore/pipeline1_batch_delta/moc_source_a/Inventory.csv"
+output_path = "/mnt/delta/bronze/inventory"
+
+df_inventory = (
+    spark.read
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .csv(inventory_path)
+)
+
+write_df_to_delta(df_inventory, output_path)
